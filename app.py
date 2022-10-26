4cb82233fd44fc3c2c9a5b658ac817a76c2e49db
AQ <anzorq@users.noreply.huggingface.co>
2022-10-26 13:42:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ pipe = StableDiffusionPipeline.from_pretrained("nitrosocke/Arcane-Diffusion", to
 if torch.cuda.is_available():
   pipe = pipe.to("cuda")
 
-def inference(prompt, guidance, steps):    
-    all_images = []
-    images = pipe([prompt] * 1, num_inference_steps=int(steps), guidance_scale=guidance, width=512, height=512).images
-    all_images.extend(images)
-    return all_images
+def inference(prompt, guidance, steps):
+    prompt = prompt + " , arcane style"
+    image = pipe(prompt, num_inference_steps=int(steps), guidance_scale=guidance, width=512, height=512).images[0]
+    return image
 
 with gr.Blocks() as demo:
     gr.HTML(
@@ -39,22 +38,22 @@ with gr.Blocks() as demo:
     with gr.Row():
         
         with gr.Column():
-            prompt = gr.Textbox(label="prompt", placeholder="(append ' , arcane style' as shown in the examples)")
+            prompt = gr.Textbox(label="prompt", placeholder="' , arcane style' is appended automatically")
             guidance = gr.Slider(label="guidance scale", value=7.5, maximum=15)
             steps = gr.Slider(label="steps", value=50, maximum=100, minimum=2)
             run = gr.Button(value="Run")
             gr.Markdown(f"Running on: {device}")
         with gr.Column():
-            gallery = gr.Gallery(height=512)
+            image_out = gr.Image(height=512)
 
-    run.click(inference, inputs=[prompt, guidance, steps], outputs=gallery)
+    run.click(inference, inputs=[prompt, guidance, steps], outputs=image_out)
     gr.Examples([
         ["jason bateman disassembling the demon core, arcane style", 7.5, 50],
         ["portrait of dwayne johnson, arcane style", 7.0, 75],
         ["portrait of a beautiful alyx vance half life, volume lighting, concept art, by greg rutkowski!!, colorful, xray melting colors!!, arcane style", 7, 50],
         ["Aloy from Horizon: Zero Dawn, half body portrait, videogame cover art, highly detailed, digital painting, artstation, concept art, smooth, detailed armor, sharp focus, beautiful face, illustration, art by Artgerm and greg rutkowski and alphonse mucha, arcane style", 7, 50],
         ["fantasy portrait painting, digital art, arcane style", 4, 30],
-    ], [prompt, guidance, steps], gallery, inference, cache_examples=torch.cuda.is_available())
+    ], [prompt, guidance, steps], image_out, inference, cache_examples=torch.cuda.is_available())
     gr.HTML('''
         <div>
             <p>Model by <a href="https://huggingface.co/nitrosocke" style="text-decoration: underline;" target="_blank">@nitrosocke</a> ❤️</p>
